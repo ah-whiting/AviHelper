f6156a93bb28a34deae3fedb55ea39fb220450a2
Alex W <runthesun@gmail.com>
2019-03-12 22:31:35 -0700

    d3 dynamic tables creation

--- public/src/app/table-display/table-display.component.ts
+++ public/src/app/table-display/table-display.component.ts
@@ -1,22 +1,94 @@
-import { Component, OnInit } from '@angular/core';
-import {NwacCsvService} from '../nwac-csv.service'
+import { Component, OnInit } from "@angular/core";
+import { NwacCsvService } from "../nwac-csv.service";
+import * as d3 from "d3";
 
 @Component({
-  selector: 'app-table-display',
-  templateUrl: './table-display.component.html',
-  styleUrls: ['./table-display.component.css']
+  selector: "app-table-display",
+  templateUrl: "./table-display.component.html",
+  styleUrls: ["./table-display.component.css"]
 })
 export class TableDisplayComponent implements OnInit {
 
-  constructor(private _nwac: NwacCsvService) { }
+  dataOptions = [
+    "temperature",
+    "precipitation",
+    "snowfall_24_hour",
+    "wind_direction",
+    "wind_speed_average"
+  ];
+  chartData: object = {};
+  nwac: any = [];
+  tables = {};
 
-  ngOnInit() {
+  constructor(private _nwac: NwacCsvService) {}
+
+  ngOnInit(){}
+
+  ngAfterViewInit(): void {
     this.getData();
   }
 
   getData() {
-    this._nwac.getData("snoqualmie-pass")
-      .subscribe(data => console.log(data));
+    this._nwac.getData("snoqualmie-pass").subscribe(data => {
+      console.log(data);
+      this.drawTable(data, this.dataOptions);
+    })
   }
 
+  arrObjToMatrix(data) {
+    let row = [];
+    let matrix = [];
+    for(let i in data) {
+      for(let j in data[i]) {
+        row.push(data[i][j]);
+      }
+      matrix.push(row);
+      row = [];
+    }
+    return matrix;
+  }
+
+  drawTable(data: Object, keys: string[]) {
+
+    keys.forEach((option, i) => {
+
+      let headers: string[] = [];
+      for (let key in data[option][0]) {
+        headers.push(key);
+      }
+
+      //create tables and headers
+      this.tables[option] = 
+        d3.select("#tableWrapper")
+          .append("div")
+            .attr("class", "col")
+            .selectAll("div")
+            .data([option])
+            .join("table")
+              .attr("class", "table");
+      this.tables[option]
+        .append("thead")
+          .append("tr")
+            .selectAll("th")
+            .data(headers)
+            .join("th")
+              .text(d => d);
+      
+      //bind and write table data
+      this.tables[option]
+        .append("tbody")
+          .selectAll("tr")
+          .data(this.arrObjToMatrix(data[option]))
+          .join("tr")
+            .selectAll("td")
+            .data(d => d)
+            .join("td")
+              .text(d => d);
+    })
+
+    
+        
+    
+
+  }
 }
